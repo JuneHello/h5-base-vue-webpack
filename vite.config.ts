eff96a82c12c29cc49154dd486c59773fe623b91
abe-119 <zhangfeng-119@gr.intra.cpic.com.cn>
2024-03-07 09:41:14 +0800

    feat: init

---
+++ vite.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig } from "vite";
+import path from "path";
+import vue from "@vitejs/plugin-vue";
+import htmlTemplate from "vite-plugin-html-template";
+import EnvironmentPlugin from "vite-plugin-environment";
+import eslintPlugin from "vite-plugin-eslint";
+import Components from "unplugin-vue-components/vite";
+
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
+export default defineConfig(({ mode }) => {
+  return {
+    resolve: {
+      alias: {
+        "@": resolve("src"),
+        "/#/": resolve("types")
+      }
+    },
+    plugins: [
+      vue(),
+      htmlTemplate(),
+      EnvironmentPlugin("all", { prefix: "VUE_APP_" }),
+      Components({
+        types: [],
+        dts: true
+      }),
+      mode === "development" &&
+        eslintPlugin({
+          include: ["src/**/*.js", "src/**/*.jsx", "src/**/*.vue", "src/**/*.tsx", "src/**/*.ts"],
+          exclude: ["./node_modules/**"],
+          cache: false
+        })
+    ]
+  };
+});
---
+++ vue.config.js
@@ -0,0 +1,26 @@
+const { defineConfig } = require("@vue/cli-service");
+const path = require("path");
+const Components = require("unplugin-vue-components/webpack");
+
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  lintOnSave: process.env.NODE_ENV === "development",
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "@": resolve("src"),
+        "/#/": resolve("types")
+      }
+    },
+    plugins: [
+      Components({
+        types: [],
+        dts: true
+      })
+    ]
+  }
+});
